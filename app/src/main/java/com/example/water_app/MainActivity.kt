b51600f241e19635647c04c224a8c8d661a9e073
정윤수 <https://github.com/whatpill1/whatpill.git>
2022-12-22 16:46:40 +0900

    status bar

--- app/src/main/java/com/example/water_app/MainActivity.kt
+++ app/src/main/java/com/example/water_app/MainActivity.kt
@@ -2,21 +2,21 @@ package com.example.water_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // 하단 탭이 눌렸을 때 화면을 전환하기 위해선 이벤트 처리하기 위해 BottomNavigationView 객체 생성
+
+        // BottomNavigationView 객체 생성
         var bnv_main = findViewById(R.id.bnv_main) as BottomNavigationView
 
-        // OnNavigationItemSelectedListener를 통해 탭 아이템 선택 시 이벤트를 처리
-        // navi_menu.xml 에서 설정했던 각 아이템들의 id를 통해 알맞은 프래그먼트로 변경하게 한다.
         bnv_main.run { setOnNavigationItemSelectedListener {
             when(it.itemId) {
+                // 프래그먼트 이동
                 R.id.first -> {
-                    // 다른 프래그먼트 화면으로 이동하는 기능
                     val homeFragment = HomeFragment()
                     supportFragmentManager.beginTransaction().replace(R.id.fl_container, homeFragment).commit()
                 }
