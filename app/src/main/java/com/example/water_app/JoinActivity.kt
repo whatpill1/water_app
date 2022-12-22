269a8867107d7295393e86dce4179f2d41a2522a
정윤수 <https://github.com/whatpill1/whatpill.git>
2022-12-22 15:49:00 +0900

    store

--- app/src/main/java/com/example/water_app/JoinActivity.kt
+++ app/src/main/java/com/example/water_app/JoinActivity.kt
@@ -3,7 +3,7 @@ package com.example.water_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Join : AppCompatActivity() {
+class JoinActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
---
+++ app/src/main/java/com/example/water_app/StoreActivity.kt
@@ -0,0 +1,11 @@
+package com.example.water_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StoreActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_store)
+    }
+}
