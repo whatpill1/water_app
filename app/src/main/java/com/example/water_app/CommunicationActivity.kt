dad39ad59a7730d44fee8208f9e3eb0cc96edf52
정윤수 <https://github.com/whatpill1/whatpill.git>
2022-12-23 11:54:49 +0900

    기부

--- app/src/main/java/com/example/water_app/CommunicationActivity.kt
+++ app/src/main/java/com/example/water_app/CommunicationActivity.kt
@@ -2,10 +2,35 @@ package com.example.water_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class CommunicationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_communication)
+
+        // BottomNavigationView 객체 생성
+        var bnv_main = findViewById(R.id.bnv_main) as BottomNavigationView
+
+        bnv_main.run { setOnNavigationItemSelectedListener {
+            when(it.itemId) {
+                // 프래그먼트 이동
+                R.id.first -> {
+                    val homeFragment = ComIntroduceFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.fl_container, homeFragment).commit()
+                }
+                R.id.second -> {
+                    val boardFragment = ComHistoryFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.fl_container, boardFragment).commit()
+                }
+                R.id.third -> {
+                    val settingFragment = ComStoryFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.fl_container, settingFragment).commit()
+                }
+            }
+            true
+        }
+            selectedItemId = R.id.first
+        }
     }
 }
