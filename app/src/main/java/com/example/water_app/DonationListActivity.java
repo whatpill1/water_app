ea16f4079a84338347e75a8e07e60d82eea806d9
안상현 <wlsdl9824@gmail.com>
2022-12-23 12:02:36 +0900

    카테고리 클릭시 이벤트 화면 만들었음

---
+++ app/src/main/java/com/example/water_app/DonationListActivity.java
@@ -0,0 +1,14 @@
+package com.example.water_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DonationListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donation_list);
+    }
+}
