66b1e23568daf5169bf084f1b4b6729f64684b16
안상현 <wlsdl9824@gmail.com>
2022-12-27 12:04:53 +0900

    삭제

--- app/src/main/java/com/example/water_app/mypage/Constants.kt
+++
@@ -1,8 +0,0 @@
-package com.example.water_app.mypage
-
-class Constants {
-
-    companion object{
-        const val BASE_URL = "http://10.1.4.122"
-    }
-}
--- app/src/main/java/com/example/water_app/mypage/SimpleApi.kt
+++
@@ -1,9 +0,0 @@
-package com.example.water_app.mypage
-
-import retrofit2.Response
-import retrofit2.http.GET
-
-interface SimpleApi {
-    @GET("test2.php")
-    suspend fun getPost() : Response<Post>
-}
