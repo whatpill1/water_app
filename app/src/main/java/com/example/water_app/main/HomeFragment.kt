42ee8274eb04dfb03de24ce32b92f194f568ff3b
안상현 <wlsdl9824@gmail.com>
2022-12-28 13:42:42 +0900

    자동 배너 완성

--- app/src/main/java/com/example/water_app/main/HomeFragment.kt
+++ app/src/main/java/com/example/water_app/main/HomeFragment.kt
@@ -3,19 +3,21 @@ package com.example.water_app.main
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 import com.example.water_app.R
 import com.example.water_app.databinding.FragmentHomeBinding
 import com.example.water_app.home.SubmitActivity
 import com.example.water_app.recyclerview.HomeAdapter
 import com.example.water_app.recyclerview.MyViewPagerAdapter
+import com.example.water_app.recyclerview.ViewPagerAdapter
 import com.example.water_app.vo.HomeData
-import com.example.water_app.vo.PostData
 
 class HomeFragment : Fragment() {
 
@@ -26,6 +28,11 @@ class HomeFragment : Fragment() {
     lateinit var mainActivity: MainActivity
 
     //뷰페이저
+    private val sliderImageHandler: Handler = Handler()
+    private val sliderImageRunnable = Runnable { binding.ivBanner.currentItem = binding.ivBanner.currentItem + 1 }
+
+
+
 
     private fun getAespaMembers(): ArrayList<Int> {
         return arrayListOf<Int>(
@@ -51,8 +58,36 @@ class HomeFragment : Fragment() {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
 
         //뷰페이저
-        binding.ivBanner.adapter = MyViewPagerAdapter(getAespaMembers()) // 어댑터 생성
-        binding.ivBanner.orientation = ViewPager2.ORIENTATION_HORIZONTAL // 방향을 가로로
+//        binding.ivBanner.adapter = MyViewPagerAdapter(getAespaMembers()) // 어댑터 생성
+//        binding.ivBanner.orientation = ViewPager2.ORIENTATION_HORIZONTAL // 방향을 가로로
+
+        val imageList = arrayListOf<Int>().apply {
+            for (i in 0..2) {
+                add(R.drawable.banner1)
+            }
+        }
+
+        val pageMarginPx = resources.getDimensionPixelOffset(R.dimen.pageMargin)
+        val pagerWidth = resources.getDimensionPixelOffset(R.dimen.pageWidth)
+        val screenWidth = resources.displayMetrics.widthPixels
+        val offsetPx = screenWidth - pageMarginPx - pagerWidth
+
+        binding.ivBanner.apply {
+            adapter = ViewPagerAdapter(imageList, binding.ivBanner)
+            offscreenPageLimit = 1
+            getChildAt(0).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
+            registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    sliderImageHandler.removeCallbacks(sliderImageRunnable)
+                    sliderImageHandler.postDelayed(sliderImageRunnable, 3000)
+                }
+            })
+            setPageTransformer { page, position ->
+                page.translationX = position * -offsetPx
+            }
+        }
+
 
 
         // MainActivity 담음
@@ -102,5 +137,15 @@ class HomeFragment : Fragment() {
         }
 
         return binding.root
+    }
+    //뷰페이저
+    override fun onResume() {
+        super.onResume()
+        sliderImageHandler.postDelayed(sliderImageRunnable, 1000)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sliderImageHandler.removeCallbacks(sliderImageRunnable)
     }
 }
