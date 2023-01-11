1368e4ab337814690acc449b2dc9c54342ebcc2b
정윤수 <https://github.com/whatpill1/whatpill.git>
2023-01-11 15:36:32 +0900

    지도 검색

--- app/src/main/java/com/example/water_app/map/KakaoAPI.kt
+++ app/src/main/java/com/example/water_app/map/KakaoAPI.kt
@@ -8,7 +8,7 @@ import retrofit2.http.Query
 interface KakaoAPI {
     @GET("v2/local/search/keyword.json") // Keyword.json의 정보를 받아옴
     fun getSearchKeyword(
-        @Header("bd5be5c79f704a2ca43de0e810f10a03") key: String, // 카카오 API 인증키
+        @Header("Authorization") key: String, // 카카오 API 인증키
         @Query("query") query: String, // 검색 키워드
         @Query("page") page: Int // 결과 페이지 번호
     ): Call<ResultSearchKeyword> // 받아온 정보가 ResultSearchKeyword 클래스의 구조로 담김
