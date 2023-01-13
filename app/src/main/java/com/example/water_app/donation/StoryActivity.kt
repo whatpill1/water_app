082efa9b10a767ac0b79565fdad804d5ba97804b
정윤수 <https://github.com/whatpill1/whatpill.git>
2023-01-13 16:08:31 +0900

    댓글 작성

--- app/src/main/java/com/example/water_app/donation/StoryActivity.kt
+++ app/src/main/java/com/example/water_app/donation/StoryActivity.kt
@@ -3,10 +3,15 @@ package com.example.water_app.donation
 import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.water_app.R
 import com.example.water_app.databinding.ActivityCommunicationBinding
 import com.example.water_app.databinding.ActivityStoryBinding
+import com.example.water_app.repository.Instance
 import com.example.water_app.user.MySharedPreferences
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class StoryActivity : AppCompatActivity() {
 
@@ -21,16 +26,39 @@ class StoryActivity : AppCompatActivity() {
         binding = ActivityStoryBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // 글 내용
         val mlrd_ttl = this.intent.extras!!.getString("mlrd_ttl")
         val mlrd_cn = this.intent.extras!!.getString("mlrd_cn")
 
-//        // 댓글
-//        val mbr_sn = MySharedPreferences.getUserSn(this).toInt()
-//        val mlrd_sn = this.intent.extras!!.getInt("mlrd_sn")
-//        val comt_cn = binding!!.edtComment.text.toString()
-
         binding.tvTitle.text = mlrd_ttl
         binding.tvContent.text = mlrd_cn
+
+        binding.btnComment!!.setOnClickListener {
+            getComment()
+        }
+    }
+
+    private fun getComment() {
+        val mbr_sn = MySharedPreferences.getUserSn(this).toInt()
+        val mlrd_sn = this.intent.extras!!.getInt("mlrd_sn")
+        val comt_cn = binding!!.edtComment.text.toString()
+
+        val call = Instance.api.getComment(mbr_sn, mlrd_sn, comt_cn)
+        call!!.enqueue(object : Callback<String?> {
+            override fun onResponse(call: Call<String?>, response: Response<String?>) {
+                if (response.isSuccessful && response.body() != null) {
+                    val joinConfirm = response.body()!!
+                    if (joinConfirm.toString() == "") {
+                        Toast.makeText(this@StoryActivity, "댓글을 작성해 주세요", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this@StoryActivity, "댓글이 작성되었습니다.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<String?>, t: Throwable) {
+                // 네
+            }
+        })
+
     }
 }
